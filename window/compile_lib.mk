aa35dd2d45912514cb86a0970d0f2d8f2163adab
Anshul Maheshwari <anshul.ffmpeg@gmail.com>
2014-07-03 19:45:21 +0530

    some more changes for building for 64 bit

--- window/compile_lib.mk
+++ window/compile_lib.mk
@@ -1,8 +1,10 @@
 SRC= ..\library\decoder.c ..\library\stream.c
 FFMPEG_LIB= avcodec.lib avformat.lib avdevice.lib avfilter.lib avutil.lib swresample.lib postproc.lib swscale.lib
 FFMPEG_VERSION_32=ffmpeg-20140609-git-6d40849-win32-dev
-FFMPEG_VERSION_64=ffmpeg-20140619-git-ab12373-win64-dev
-INCLUDE= /I..\library /I3party\$(FFMPEG_VERSION_32)\include /I.
+FFMPEG_VERSION_64=ffmpeg-20140702-git-42a92a2-win64-dev
+INCLUDE_64= /I3rdparty\$(FFMPEG_VERSION_64)\include
+INCLUDE_32= /I3rdparty\$(FFMPEG_VERSION_32)\include
+INCLUDE= /I..\library  /I.
 INCLUDE= $(INCLUDE) /I"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\include"
 OUTPUT_32=libstream_32.dll
 OUTPUT_64=libstream_64.dll
@@ -10,9 +12,9 @@ OUTPUT_64=libstream_64.dll
 all: libstream_32 libstream_64
 	
 libstream_64:
-	$(CC) $(INCLUDE) $(SRC) /link /DLL /OUT:$(OUTPUT_64) /LIBPATH:3party\$(FFMPEG_VERSION_64)\lib $(FFMPEG_LIB)
+	$(CC) $(INCLUDE_64) $(INCLUDE) $(SRC) /link /DLL /OUT:$(OUTPUT_64) /LIBPATH:3rdparty\$(FFMPEG_VERSION_64)\lib $(FFMPEG_LIB)
 	
 libstream_32: 
-	$(CC) $(INCLUDE) $(SRC) /link /DLL  /MACHINE:X64  /OUT:$(OUTPUT_32) /LIBPATH:3party\$(FFMPEG_VERSION_32)\lib $(FFMPEG_LIB)
+	$(CC) $(INCLUDE) $(INCLUDE_32) $(SRC) /link /DLL  /MACHINE:X64  /OUT:$(OUTPUT_32) /LIBPATH:3rdparty\$(FFMPEG_VERSION_32)\lib $(FFMPEG_LIB)
 
 
--- window/compile_testapp.mk
+++ window/compile_testapp.mk
@@ -2,7 +2,7 @@ SRC=..\test.cpp
 INCLUDE=/I..\library /I"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\include"
 
 all:
-	cl /EHsc $(INCLUDE) $(SRC) libstream_32.lib
+	cl /EHsc $(INCLUDE) $(SRC) libstream_64.lib
 
 clean:
 	del *.obj test.exe
