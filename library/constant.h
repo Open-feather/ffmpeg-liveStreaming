76300a95a4c1dcd75875202cdc8036fddfcd24e9
Anshul <er.anshul.maheshwari@gmail.com>
2014-09-16 01:09:19 -0700

    added library constant.h

---
+++ library/constant.h
@@ -0,0 +1,15 @@
+/*
+ * constant.h
+ *
+ *  Created on: Sep 7, 2014
+ *      Author: anshul
+ */
+
+#ifndef CONSTANT_H_
+#define CONSTANT_H_
+
+enum DuplicateFormat {
+	SIDE_BY_SIDE, TOP_N_BOTTOM
+};
+
+#endif /* CONSTANT_H_ */
